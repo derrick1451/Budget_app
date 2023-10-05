9c219ff4c07d07f4f60a88fe58f08f71ed43d97c
Derrick1451 <haylederrick14@gmail.com>
2023-10-05 13:35:43 +0300

    Modify controller files

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,26 @@
+class GroupsController < ApplicationController
+  before_action :authenticate_user!
+  load_and_authorize_resource
+  def index
+    @groups = current_user.groups.order(created_at: :desc)
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = current_user.groups.new(group_params)
+    if @group.save
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
+  end
+end
