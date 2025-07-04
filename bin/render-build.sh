46ff6dbc2eb831d63b343e521aefaeb0ea659b50
derrick <haylederrick14@gmail.com>
2025-07-04 15:11:53 +0300

    redeploy project

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
+
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+bin/rails assets:precompile
+bin/rails assets:clean
+
+bin/rails db:migrate
